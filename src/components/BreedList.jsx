bd788fa9ff897ebd92196fe8088190bd174c6d58
Rohith Ramanathan <rohram04@gmail.com>
2023-10-01 16:07:21 -0500

    filtering before map

--- src/components/BreedList.jsx
+++ src/components/BreedList.jsx
@@ -25,33 +25,34 @@ export default function BreedList({
         value={searchText}
       ></input>
       <ul className="py-2 space-y-1.5 h-inherit overflow-y-scroll no-scrollbar snap-y">
-        {breeds.map((breed) => {
-          if (!breed.includes(searchText)) return;
-          return (
-            <li className="snap-center">
-              <button
-                onClick={() => {
-                  if (selectedBreeds.includes(breed))
-                    return setSelectedBreeds((previouslySelectedBreeds) =>
-                      previouslySelectedBreeds.filter((x) => x !== breed)
-                    );
-                  setSelectedBreeds((previouslySelectedBreeds) => [
-                    ...previouslySelectedBreeds,
-                    breed,
-                  ]);
-                }}
-                className={
-                  "p-2 rounded-md shadow-md w-full text-left hover:cursor- sm:text-xl" +
-                  (selectedBreeds.includes(breed)
-                    ? " bg-blue-800 hover:opacity-80 text-white"
-                    : " hover:bg-slate-200")
-                }
-              >
-                {breed}
-              </button>
-            </li>
-          );
-        })}
+        {breeds
+          .filter((breed) => breed.includes(searchText))
+          .map((breed) => {
+            return (
+              <li className="snap-center">
+                <button
+                  onClick={() => {
+                    if (selectedBreeds.includes(breed))
+                      return setSelectedBreeds((previouslySelectedBreeds) =>
+                        previouslySelectedBreeds.filter((x) => x !== breed)
+                      );
+                    setSelectedBreeds((previouslySelectedBreeds) => [
+                      ...previouslySelectedBreeds,
+                      breed,
+                    ]);
+                  }}
+                  className={
+                    "p-2 rounded-md shadow-md w-full text-left hover:cursor- sm:text-xl" +
+                    (selectedBreeds.includes(breed)
+                      ? " bg-blue-800 hover:opacity-80 text-white"
+                      : " hover:bg-slate-200")
+                  }
+                >
+                  {breed}
+                </button>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
